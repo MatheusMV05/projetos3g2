6a650473ebc077b19287e9325eae54798dd172bd
Matheus Verissimo <matheusmverissimo820@gmail.com>
2025-02-26 14:36:39 -0300

    Adicionei a base do springboot

---
+++ src/main/java/com/brasfi/site_institucional/SiteInstitucionalApplication.java
@@ -0,0 +1,13 @@
+package com.brasfi.site_institucional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteInstitucionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteInstitucionalApplication.class, args);
+	}
+
+}
