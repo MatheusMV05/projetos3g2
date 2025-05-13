31758c81fa3fb8e06e9ad9bef23d938b27bef70d
JuliaTBarros <162466823+JuliaTBarros@users.noreply.github.com>
2025-05-12 21:06:37 -0300

    Refactor TrabalheConosco component layout and clean up CSS styles

--- Frontend/src/pages/TrabalheConosco/TrabalheConosco.tsx
+++ Frontend/src/pages/TrabalheConosco/TrabalheConosco.tsx
@@ -2,17 +2,7 @@ import React from 'react';
 import CallToAction from '../../components/CallToAction/CallToAction';
 
 const TrabalheConosco: React.FC = () => {
-	return (
-		<div
-			style={{
-				display: 'flex',
-				justifyContent: 'center',
-				padding: '40px 16px',
-			}}
-		>
-			<CallToAction />
-		</div>
-	);
+	return <CallToAction />;
 };
 
 export default TrabalheConosco;
