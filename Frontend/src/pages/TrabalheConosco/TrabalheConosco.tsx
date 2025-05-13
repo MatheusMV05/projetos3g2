d16a686d007c7d81deff7c369d90d1671c92573d
JuliaTBarros <162466823+JuliaTBarros@users.noreply.github.com>
2025-05-13 10:18:00 -0300

    Add framer-motion for animations, implement Testimonial component, and enhance Footer styles

--- Frontend/src/pages/TrabalheConosco/TrabalheConosco.tsx
+++ Frontend/src/pages/TrabalheConosco/TrabalheConosco.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import CallToAction from '../../components/CallToAction/CallToAction';
+import Testimonial from '../../components/Testimonial/Testimonial'; // <-- novo import
 
 const TrabalheConosco: React.FC = () => {
-	return <CallToAction />;
+	return (
+		<>
+			<CallToAction />
+			<Testimonial />
+		</>
+	);
 };
 
 export default TrabalheConosco;
