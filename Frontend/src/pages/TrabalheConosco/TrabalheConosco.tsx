247b81318dbb3022b33a5522fcb55edf511298c4
JuliaTBarros <162466823+JuliaTBarros@users.noreply.github.com>
2025-05-13 11:44:34 -0300

    Add Partners component and styles to TrabalheConosco page

--- Frontend/src/pages/TrabalheConosco/TrabalheConosco.tsx
+++ Frontend/src/pages/TrabalheConosco/TrabalheConosco.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import CallToAction from '../../components/CallToAction/CallToAction';
-import Testimonial from '../../components/Testimonial/Testimonial'; // <-- novo import
+import Testimonial from '../../components/Testimonial/Testimonial';
+import Partners from '../../components/Partners/Partners';
 
 const TrabalheConosco: React.FC = () => {
 	return (
 		<>
 			<CallToAction />
 			<Testimonial />
+			<Partners />
 		</>
 	);
 };
