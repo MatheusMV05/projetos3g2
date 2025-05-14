04a1e9312ec1a798f8efb5801bc83d89e5560aeb
JuliaTBarros <162466823+JuliaTBarros@users.noreply.github.com>
2025-05-14 11:17:54 -0300

    Add SustainableFinanceCourses component and integrate into Servicos page; update package.json and package-lock.json for new dependencies

--- Frontend/src/pages/Servicos/Servicos.tsx
+++ Frontend/src/pages/Servicos/Servicos.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import SustainableProjectsSection from '../../components/Projects/SustainableProjectsSection';
 import Metrics from '../../components/Metrics/Metrics';
+import SustainableFinanceCourses from '../../components/SustainableFinanceCourses/SustainableFinanceCourses'
 
 const Servicos: React.FC = () => {
 	return (
 		<main>
 			<SustainableProjectsSection />
 			<Metrics />
+			<SustainableFinanceCourses />
 		</main>
 	);
 };
