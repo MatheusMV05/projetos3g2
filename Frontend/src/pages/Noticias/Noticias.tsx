f53256891904f665aaef7550fad7825d1bf92f2d
Fátima Beatriz Moraes <fbmmca@cesar.school>
2025-05-16 23:26:10 -0300

    Pagina de Noticias finalizada, falta so algumas alteracoes no css

--- Frontend/src/pages/Noticias/Noticias.tsx
+++ Frontend/src/pages/Noticias/Noticias.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import NoticiasIntro from '../../components/NoticiasIntro/NoticiasIntro';
+import Blog from '../../components/Blog/Blog';
+import Recursos from '../../components/Recursos/Recursos'; // <-- importado aqui
 
 const Noticias: React.FC = () => {
 	return (
 		<div style={{ backgroundColor: '#fff', padding: '2rem' }}>
 			<h1>Notícias</h1>
 			<p>Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
+			
 			<NoticiasIntro />
+			<Blog />
+			<Recursos /> {/* <-- adicionado aqui */}
 		</div>
 	);
 };
