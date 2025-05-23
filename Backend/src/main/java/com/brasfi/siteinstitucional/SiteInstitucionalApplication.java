a5ce10532cc03310e8f4cbfd1200765067869a5f
Matheus Verissimo <matheusmverissimo820@gmail.com>
2025-05-22 23:29:53 -0300

    Revert "Merge pull request #4 from MatheusMV05/api"
    This reverts commit 9789ecb936b8c5a58f5c79ef1fd268bfca4b4754, reversing
    changes made to bfbfaffa020b51b738aa58dd8dfbab819053a098.

--- Backend/src/main/java/com/brasfi/siteinstitucional/SiteInstitucionalApplication.java
+++ Backend/src/main/java/com/brasfi/siteinstitucional/SiteInstitucionalApplication.java
@@ -2,10 +2,8 @@ package com.brasfi.siteinstitucional;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class SiteInstitucionalApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SiteInstitucionalApplication.class, args);
