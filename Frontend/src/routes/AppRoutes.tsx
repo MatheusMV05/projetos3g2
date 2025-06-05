c7c97c28067f821d2bf887091f6b89851dcdf3f0
JuliaTBarros <162466823+JuliaTBarros@users.noreply.github.com>
2025-06-05 18:56:50 -0300

    Add CancelarInscricaoPage component and routing for newsletter cancellation

--- Frontend/src/routes/AppRoutes.tsx
+++ Frontend/src/routes/AppRoutes.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 import MainLayout from '../layouts/MainLayout';
 import PaginaDinamica from '../pages/PaginaDinamica/PaginaDinamica.tsx'; // 1. Import the new component
 import LayoutADM from '../layouts/LayoutADM'; // novo layout
@@ -12,31 +12,33 @@ import Noticias from '../pages/Noticias/Noticias';
 import Equipe from '../pages/Equipe/Equipe';
 import Conteudo from '../pages/Conteudo/Conteudo'
 import PostEditor from '../pages/PostEditor/PostEditor'
+import CancelarInscricaoPage from '../pages/CancelarInscricao/CancelarInscricaoPage'; // Importe a nova página
 
 const AppRoutes: React.FC = () => {
-	return (
-		<Routes>
-			{/* Rotas públicas */}
-			<Route element={<MainLayout />}>
-				<Route path="/" element={<Home />} />
-				<Route path="/pagina/:slug" element={<PaginaDinamica />} /> {/* 2. Add this new route */}
-				<Route path="/trabalhe-conosco" element={<TrabalheConosco />} />
-				<Route path="/evento" element={<Evento />} />
-				<Route path="/servicos" element={<Servicos />} />
-				<Route path="/sobre" element={<Sobre />} />
-				<Route path="/noticias" element={<Noticias />} />
-			</Route>
+    return (
+        <Routes>
+            {/* Rotas públicas */}
+            <Route element={<MainLayout/>}>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/pagina/:slug" element={<PaginaDinamica/>}/> {/* 2. Add this new route */}
+                <Route path="/trabalhe-conosco" element={<TrabalheConosco/>}/>
+                <Route path="/evento" element={<Evento/>}/>
+                <Route path="/servicos" element={<Servicos/>}/>
+                <Route path="/sobre" element={<Sobre/>}/>
+                <Route path="/noticias" element={<Noticias/>}/>
+                <Route path="/newsletter/cancelar" element={<CancelarInscricaoPage/>}/> {/* Adicione esta rota */}
+            </Route>
 
-			{/* Rotas administrativas */}
-			<Route element={<LayoutADM />}>
-				<Route path="/equipe" element={<Equipe />} />
-				<Route path="/conteudo" element={<Conteudo />} />
-			</Route>
+            {/* Rotas administrativas */}
+            <Route element={<LayoutADM/>}>
+                <Route path="/equipe" element={<Equipe/>}/>
+                <Route path="/conteudo" element={<Conteudo/>}/>
+            </Route>
 
-			<Route path="/post-editor" element={<PostEditor />} />
+            <Route path="/post-editor" element={<PostEditor/>}/>
 
-		</Routes>
-	);
+        </Routes>
+    );
 };
 
 export default AppRoutes;
